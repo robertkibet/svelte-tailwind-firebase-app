f7c8d5749211c934c98cc0afe928f65f01887114
Robert Kibet <kipronokrobert@gmail.com>
2023-01-13 17:34:45 +0300

    refactor(test): update playwright configs

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,13 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 import { devices } from '@playwright/test';
 
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 const config: PlaywrightTestConfig = {
 	testDir: './tests',
+
 	/* Maximum time one test can run for. */
 	timeout: 30 * 1000,
 	expect: {
@@ -15,16 +17,22 @@ const config: PlaywrightTestConfig = {
 		 */
 		timeout: 5000
 	},
+
 	/* Run tests in files in parallel */
 	fullyParallel: true,
+
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
 	forbidOnly: !!process.env.CI,
+
 	/* Retry on CI only */
 	retries: process.env.CI ? 2 : 0,
+
 	/* Opt out of parallel tests on CI. */
 	workers: process.env.CI ? 1 : undefined,
+
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
-	// reporter: 'html',
+	reporter: process.env.CI ? 'dot' : 'list',
+
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
@@ -32,13 +40,8 @@ const config: PlaywrightTestConfig = {
 		/* Base URL to use in actions like `await page.goto('/')`. */
 		baseURL: 'http://localhost:5173',
 
-		launchOptions: {
-			slowMo: parseInt(process.env.SLOW_MO || '0')
-		},
-
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-		trace: 'on-first-retry',
-		video: 'retain-on-failure'
+		trace: 'on-first-retry'
 	},
 
 	/* Configure projects for major browsers */
@@ -46,7 +49,10 @@ const config: PlaywrightTestConfig = {
 		{
 			name: 'chromium',
 			use: {
-				...devices['Desktop Chrome']
+				...devices['Desktop Chrome'],
+				launchOptions: {
+					args: ['--disable-web-security']
+				}
 			}
 		},
 
@@ -56,23 +62,22 @@ const config: PlaywrightTestConfig = {
 				...devices['Desktop Firefox']
 			}
 		},
+
 		{
-			name: 'Mobile Safari',
+			name: 'webkit',
 			use: {
-				...devices['iPhone 12']
+				...devices['Desktop Safari']
 			}
 		}
 	],
-
-	/* Folder for test artifacts such as screenshots, videos, traces, etc. */
-	// outputDir: 'test-results/',
 
 	/* Run your local dev server before starting the tests */
 	webServer: {
 		command: 'npm run dev',
 		port: 5173,
-		reuseExistingServer: !process.env.CI
+		reuseExistingServer: true
 	}
 };
 
 export default config;
+
