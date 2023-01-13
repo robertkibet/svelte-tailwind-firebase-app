85e5ee4010efdd7819fb23558fff685723ae9603
Robert Kibet <kipronokrobert@gmail.com>
2023-01-13 18:05:09 +0300

    refactor(tests): check for page title

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,74 +1,86 @@
 import { expect, test } from '@playwright/test';
 
-test('Should have nav with 2 links', async ({ page }) => {
-	await page.goto('/');
-	const nav = page.locator('nav');
-
-	expect(await nav.isVisible()).toBe(true);
-
-	const navLinks = nav.locator('a');
-
-	expect(await navLinks.count()).toBe(2);
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://demo.playwright.dev/todomvc');
 });
 
-test('Nav should have index page and about page links', async ({ page }) => {
-	await page.goto('/');
-	const nav = page.locator('nav');
-
-	const navLinks = nav.locator('a');
-	const homePage = await navLinks.nth(0).getAttribute('href');
-	const aboutPage = await navLinks.nth(1).getAttribute('href');
-
-	expect(homePage).toBe('/');
-	expect(aboutPage).toBe('/about');
-});
-
-test('Homepage should have input field with add todo button', async ({ page }) => {
+  test('Loads playwright page', async ({ page }) => {
+    // navigate to playwright page
 	await page.goto('/');
 
-	await page.waitForTimeout(1000);
+	expect(await page.title()).toContain('Svelte Todo App');
+  });
 
-	const input = await page.locator('[data-todos="todo-input"]');
-	const button = await page.locator('[data-todos="add-todo"]');
 
-	await page.waitForTimeout(1000);
+// test('Should have nav with 2 links', async ({ page }) => {
+// 	await page.goto('/');
+// 	const nav = page.locator('nav');
 
-	expect(await input.isVisible()).toBe(true);
-	expect(await button.isVisible()).toBe(true);
+// 	expect(await nav.isVisible()).toBe(true);
 
-	const inputType = await input.getAttribute('type');
-	expect(inputType).toBe('text');
+// 	const navLinks = nav.locator('a');
 
-	const buttonText = await button.innerText();
-	expect(buttonText.toLowerCase().trim()).toBe('add todo'.trim());
-});
+// 	expect(await navLinks.count()).toBe(2);
+// });
 
-test('Should add todo to list', async ({ page }) => {
-	await page.goto('/');
+// test('Nav should have index page and about page links', async ({ page }) => {
+// 	await page.goto('/');
+// 	const nav = page.locator('nav');
 
-	await page.waitForTimeout(1000);
+// 	const navLinks = nav.locator('a');
+// 	const homePage = await navLinks.nth(0).getAttribute('href');
+// 	const aboutPage = await navLinks.nth(1).getAttribute('href');
 
-	const input = await page.locator('[data-todos="todo-input"]');
-	const button = await page.locator('[data-todos="add-todo"]');
+// 	expect(homePage).toBe('/');
+// 	expect(aboutPage).toBe('/about');
+// });
 
-	await page.waitForTimeout(1000);
+// test('Homepage should have input field with add todo button', async ({ page }) => {
+// 	await page.goto('/');
 
-	const todoList = page.locator('[data-todos="todos"]');
-	const todoItems = await todoList.locator('[data-todos="todo-item"]');
+// 	await page.waitForTimeout(1000);
 
-	const todoListLength = await todoItems.count();
+// 	const input = await page.locator('[data-todos="todo-input"]');
+// 	const button = await page.locator('[data-todos="add-todo"]');
 
-	const randomId = await page.evaluate(() => crypto.randomUUID());
+// 	await page.waitForTimeout(1000);
 
-	await input.fill(`Test ${randomId}`);
-	await button.click();
+// 	expect(await input.isVisible()).toBe(true);
+// 	expect(await button.isVisible()).toBe(true);
 
-	await page.waitForTimeout(1000);
+// 	const inputType = await input.getAttribute('type');
+// 	expect(inputType).toBe('text');
 
-	const todoItemsUpdated = await todoList.locator('[data-todos="todo-item"]');
-	const todoListLengthUpdated = await todoItemsUpdated.count();
+// 	const buttonText = await button.innerText();
+// 	expect(buttonText.toLowerCase().trim()).toBe('add todo'.trim());
+// });
 
-	expect(todoListLength).toBeGreaterThan(0);
+// test('Should add todo to list', async ({ page }) => {
+// 	await page.goto('/');
 
-	expect(todoListLengthUpdated).toBeGreaterThan(todoListLength);
-});
+// 	await page.waitForTimeout(1000);
+
+// 	const input = await page.locator('[data-todos="todo-input"]');
+// 	const button = await page.locator('[data-todos="add-todo"]');
+
+// 	await page.waitForTimeout(1000);
+
+// 	const todoList = page.locator('[data-todos="todos"]');
+// 	const todoItems = await todoList.locator('[data-todos="todo-item"]');
+
+// 	const todoListLength = await todoItems.count();
+
+// 	const randomId = await page.evaluate(() => crypto.randomUUID());
+
+// 	await input.fill(`Test ${randomId}`);
+// 	await button.click();
+
+// 	await page.waitForTimeout(1000);
+
+// 	const todoItemsUpdated = await todoList.locator('[data-todos="todo-item"]');
+// 	const todoListLengthUpdated = await todoItemsUpdated.count();
+
+// 	expect(todoListLength).toBeGreaterThan(0);
+
+// 	expect(todoListLengthUpdated).toBeGreaterThan(todoListLength);
+// });
