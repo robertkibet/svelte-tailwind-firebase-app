454495302f8255043151b9cbae84ed4adaeeca44
Robert Kibet <kipronokrobert@gmail.com>
2023-01-13 11:00:51 +0300

    feat: add tailwind and configure pages

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import '../app.css';
+</script>
+
+<nav class="flex justify-between">
+	<a
+		href="/"
+		class="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900"
+		>TODO App</a
+	>
+	<a
+		href="/about"
+		class="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900"
+		>Learn more</a
+	>
+</nav>
+
+<slot />
